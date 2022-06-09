49bc01f3aa43e9574ab9e7a53377b21ac2d32e0d
dduartevla <deboraduarte@ice.ufjf.br>
2022-06-08 22:44:10 -0300

    Criação classe EstatisticasRepository.

---
+++ app/src/main/java/br/ufjf/dcc196/izabel/estatisticasdebasquete/EstatisticasRepository.java
@@ -0,0 +1,28 @@
+package br.ufjf.dcc196.izabel.estatisticasdebasquete;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class EstatisticasRepository {
+
+    private Context context;
+    private SharedPreferences preferences;
+    private final String PREFERENCES_NAME = "br.ufjf.dcc196.izabel.estatisticasbasquete";
+    private final String PONTOS_KEY = "PONTOS_KEY";
+
+    public EstatisticasRepository(Context context){
+        this.context = context;
+        preferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+    }
+
+    public Integer getPontos(){
+        return preferences.getInt(PONTOS_KEY,0);
+    }
+
+    public void setPontos(Integer pontos){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt(PONTOS_KEY, pontos);
+        editor.apply();
+        editor.apply();
+    }
+}
